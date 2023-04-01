2dabb0b0906a5d1af6000c9c0e992a782d1103cc
Abdelilah_OU <gxgamer710@gmail.com>
2023-04-01 14:50:55 +0000

    delete other pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,24 @@
 <script setup lang="ts">
-const counter = ref<number>(0);
+import { useCounterStore } from "~~/stores/counter";
+import { storeToRefs } from "pinia";
+
+definePageMeta({
+  layout: "index",
+});
+
+const counterStore = useCounterStore();
+const { counter } = storeToRefs(counterStore);
 </script>
 
 <template>
-  <main class="w-full h-full flex justify-center items-center">
+  <main class="w-full h-full flex flex-col gap-2 justify-center items-center">
     <button
       class="bg-red-300 w-fit rounded-md px-4 py-2 text-white"
-      @click="counter++"
+      @click="counterStore.updateCounter(5)"
     >
       Increment
     </button>
+
     {{ counter }}
-    <NuxtLink to="/student"> Go </NuxtLink>
   </main>
 </template>
